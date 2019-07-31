b6ddb267c84b250b48f7b4dc3030033ab45a4656
tiemonl <tiemon.liam@gmail.com>
2019-07-30 20:16:07 -0400

    GH-24: seperated cli from main gui as two programs

---
+++ fisherCLI/Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace fisherCLI {
+	public partial class Form1 : Form {
+		#region Declarations
+		
+		private int bait;
+		private static bool steam;
+		private static Point locationStartButton;
+		private static Point locationCloseShellDialogBox;
+		private static Point locationTradeFishButton;
+		private static Point locationCloseItGotAwayButton;
+		private static Point locationTimerCaughtFish;
+		private static Point locationJunkItem;
+		private static Point locationTopLeftWeightScreenshot;
+		private static Point locationBottomRightWeightScreenshot;
+		private static Point location100Position;
+
+		Color startButtonGreen = Color.FromArgb(155, 208, 30);
+		Color castButtonBlue = Color.FromArgb(030, 170, 208);
+		Color colorCloseItGotAwayButton = Color.FromArgb(030, 170, 208);
+		Color colorTimerCaughtFishKong = Color.FromArgb(56, 255, 56);
+		Color colorTimerCaughtFishSteam = Color.FromArgb(59, 255, 59);
+		Color colorJunkItem = Color.FromArgb(255, 255, 255);
+		Color oneHundredCatchColor = Color.FromArgb(77, 254, 0);
+
+		MethodHelper helper;
+		#endregion
+
+		public Form1(int bait) {
+			InitializeComponent();
+			this.bait = bait;
+
+			steam = false;
+
+			backgroundThread.WorkerReportsProgress = true;
+			backgroundThread.WorkerSupportsCancellation = true;
+
+			setRod();
+			findCastCatchLocation();
+			startFishing();
+		}
+
+		/// <summary>
+		/// This determines which radio button was clicked.
+		/// Each rod has a different timing.
+		/// The timing is the time it takes for the casting bar to go from its max value, down to its lowest point and back up to its max value.
+		/// I use this method of fishing as it has netted better results over clicking cast as soon as the program detects the casting bar at its max value.
+		/// </summary>
+		private void setRod() {
+			helper = new MethodHelper(steam, 1000); //TODO: change ctor to not use rodType
+		}
+
+
+		private void findCastCatchLocation() {
+			locationStartButton = helper.FindColor(startButtonGreen);
+			locationStartButton = helper.getScreenLocationPoint(locationStartButton);
+			locationTradeFishButton = new Point(locationStartButton.X, locationStartButton.Y - 15);
+			locationCloseShellDialogBox = new Point(locationStartButton.X + 265, locationStartButton.Y - 325);
+			locationCloseItGotAwayButton = new Point(locationStartButton.X + 30, locationStartButton.Y - 100);
+			locationTimerCaughtFish = new Point(locationStartButton.X - 200, locationStartButton.Y - 70);
+			locationJunkItem = new Point(locationStartButton.X + 100, locationStartButton.Y - 155);
+			locationTopLeftWeightScreenshot = new Point(locationStartButton.X - 25, locationStartButton.Y - 130);
+			locationBottomRightWeightScreenshot = new Point(locationStartButton.X + 160, locationStartButton.Y - 50);
+			location100Position = new Point(locationStartButton.X + 373, locationStartButton.Y - 81);
+		}
+
+		private void startFishing() {
+			backgroundThread.RunWorkerAsync();
+		}
+
+		private void backgroundThread_DoWork(object sender, DoWorkEventArgs e) {
+			BackgroundWorker worker = sender as BackgroundWorker;
+			int baitToUse = bait;
+			int i = 0;
+			int baitUsed = 0;
+			while (i < baitToUse) {
+				if (worker.CancellationPending == true) {
+					e.Cancel = true;
+					break;
+				} else {
+					//Performs cast
+					bool caughtFish = true;
+					bool fishGetAway = true;
+					++baitUsed;
+					Invoke(new Action(() => Refresh()));
+					Invoke(new Action(() => helper.startCast(locationStartButton)));
+					Invoke(new Action(() => Cursor.Position = locationTimerCaughtFish));
+					while (caughtFish) {
+						if (worker.CancellationPending == true) {
+							e.Cancel = true;
+							break;
+						}
+						Invoke(new Action(() => Refresh()));
+						//performs cast
+						Color color = helper.GetPixelColor(locationTimerCaughtFish);
+						//if (color == colorTimerCaughtFishKong || color == colorTimerCaughtFishSteam) {
+						if (helper.AreColorsSimilar(color, colorTimerCaughtFishKong, 20)) {
+							if (worker.CancellationPending == true) {
+								e.Cancel = true;
+								break;
+							}
+							Invoke(new Action(() => Refresh()));
+							Invoke(new Action(() => helper.catchFish(location100Position, oneHundredCatchColor)));
+							Thread.Sleep(5000);
+							while (fishGetAway) {
+
+								//fish caught
+								if (worker.CancellationPending == true) {
+									e.Cancel = true;
+									break;
+								}
+								if (helper.GetPixelColor(locationTradeFishButton) == startButtonGreen) {
+									Invoke(new Action(() => Refresh()));
+									//helper.getFishWeight(locationTopLeftWeightScreenshot);
+									Invoke(new Action(() => helper.tradeItemThenCloseClick(locationTradeFishButton, locationCloseShellDialogBox)));
+									fishGetAway = false;
+
+								}
+								//fish got away
+								else if (helper.GetPixelColor(locationCloseItGotAwayButton) == colorCloseItGotAwayButton) {
+									Invoke(new Action(() => Refresh()));
+									helper.fishGotAwayClick(locationCloseItGotAwayButton);
+									fishGetAway = false;
+								}
+							}
+							caughtFish = false;
+						}
+						// caught junk
+						else if (helper.GetPixelColor(locationJunkItem) == colorJunkItem) {
+							Invoke(new Action(() => Refresh()));
+							Invoke(new Action(() => helper.tradeItemThenCloseClick(locationTradeFishButton, locationCloseShellDialogBox)));
+							caughtFish = false;
+						}
+					}
+					++i;
+				}
+			}
+			backgroundThread.CancelAsync();
+		}
+	}
+}
+
---
+++ fisherCLI/MethodHelper.cs
@@ -0,0 +1,261 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace fisherCLI {
+	class MethodHelper {
+
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		/// <param name="usingSteam">Determines whether the user is using Steam or Kongregate for the program.</param>
+		/// <param name="rod">Determines which fishing rod the user is using to fish</param>
+		public MethodHelper(bool usingSteam, int rod) {
+			steam = usingSteam;
+			rodType = rod;
+		}
+
+		public partial class NativeMethods {
+
+			/// Return Type: BOOL->int
+			///fBlockIt: BOOL->int
+			[System.Runtime.InteropServices.DllImportAttribute("user32.dll", EntryPoint = "BlockInput")]
+			[return: System.Runtime.InteropServices.MarshalAsAttribute(System.Runtime.InteropServices.UnmanagedType.Bool)]
+			public static extern bool BlockInput([System.Runtime.InteropServices.MarshalAsAttribute(System.Runtime.InteropServices.UnmanagedType.Bool)] bool fBlockIt);
+
+
+			[DllImport("user32.dll")]
+			public static extern bool GetCursorPos(ref Point lpPoint);
+
+			[DllImport("user32")]
+			public static extern int SetCursorPos(int x, int y);
+
+			[DllImport("gdi32.dll", CharSet = CharSet.Auto, SetLastError = true, ExactSpelling = true)]
+			public static extern int BitBlt(IntPtr hDC, int x, int y, int nWidth, int nHeight, IntPtr hSrcDC, int xSrc, int ySrc, int dwRop);
+
+			[DllImport("user32.dll")]
+			public static extern IntPtr GetDC(IntPtr hwnd);
+
+			[DllImport("user32.dll")]
+			public static extern Int32 ReleaseDC(IntPtr hwnd, IntPtr hdc);
+
+			[DllImport("gdi32.dll")]
+			public static extern uint GetPixel(IntPtr hdc, int nXPos, int nYPos);
+
+			[DllImport("user32.dll")]
+			public static extern void mouse_event(int dwFlags, int dx, int dy, int dwData, int dwExtraInfo);
+		}
+		private const int MOUSEEVENTF_MOVE = 0x0001;
+		private const int MOUSEEVENTF_LEFTDOWN = 0x0002;
+		private const int MOUSEEVENTF_LEFTUP = 0x0004;
+		private const int MOUSEEVENTF_RIGHTDOWN = 0x0008;
+		private const int MOUSEEVENTF_RIGHTUP = 0x0010;
+		private const int MOUSEEVENTF_MIDDLEDOWN = 0x0020;
+		private const int MOUSEEVENTF_MIDDLEUP = 0x0040;
+		private const int MOUSEEVENTF_ABSOLUTE = 0x8000;
+
+		private int screenNumContainingGame;
+		private bool steam;
+		private int rodType;
+	
+
+		public static void LeftClick(Point point) {
+			NativeMethods.mouse_event(MOUSEEVENTF_LEFTDOWN, point.X, point.Y, 0, 0);
+			NativeMethods.mouse_event(MOUSEEVENTF_LEFTUP, point.X, point.Y, 0, 0);
+		}
+
+		//----------------------------------------------------------------------------------------------------------//
+		//-------------------------------------------Screen Pixel methods-------------------------------------------//
+		//----------------------------------------------------------------------------------------------------------//
+
+
+		public Color GetPixelColor(Point location) {
+			int x = location.X;
+			int y = location.Y;
+			IntPtr hdc = NativeMethods.GetDC(IntPtr.Zero);
+			uint pixel = NativeMethods.GetPixel(hdc, x, y);
+			NativeMethods.ReleaseDC(IntPtr.Zero, hdc);
+			Color color = Color.FromArgb((int)(pixel & 0x000000FF),
+						 (int)(pixel & 0x0000FF00) >> 8,
+						 (int)(pixel & 0x00FF0000) >> 16);
+			return color;
+		}
+
+		public bool AreColorsSimilar(Color c1, Color c2, int tolerance) {
+			return Math.Abs(c1.R - c2.R) < tolerance &&
+				   Math.Abs(c1.G - c2.G) < tolerance &&
+				   Math.Abs(c1.B - c2.B) < tolerance;
+		}
+
+		public Dictionary<int, Bitmap> GetScreenShot(bool saveScreenshot) {
+			//Using `SystemInformation.VirtualScreen` rather than `Screen.PrimaryScreen` 
+			//ensures that the program can look at all screens for the start button.
+			Dictionary<int, Bitmap> results = new Dictionary<int, Bitmap>();
+			int i = 0;
+			foreach (var screen in Screen.AllScreens) {
+
+				Bitmap result = new Bitmap(screen.Bounds.Width, screen.Bounds.Height);
+				{
+					using (Graphics gfx = Graphics.FromImage(result)) {
+						gfx.CopyFromScreen(screen.Bounds.X, screen.Bounds.Y, 0, 0, result.Size);
+						results.Add(i, result);
+					}
+					if (saveScreenshot) {
+						result.Save(screen.DeviceName.ToString().Trim('\\') + ".png", ImageFormat.Png);
+					}
+				}
+				++i;
+			}
+			return results;
+		}
+
+		public Point FindColor(Color color) {
+
+			int searchValue = color.ToArgb();
+			Point result = new Point();
+			foreach (var screen in GetScreenShot(false)) {
+				using (Bitmap bmp = screen.Value) {
+					for (int x = 0; x < bmp.Width; x++) {
+						for (int y = 0; y < bmp.Height; y++) {
+							//Cursor.Position = new Point(x, y);
+							if (searchValue.Equals(bmp.GetPixel(x, y).ToArgb())) {
+								screenNumContainingGame = screen.Key;
+								return result = new Point(x + 30, y);
+							}
+						}
+					}
+				}
+			}
+			return result;
+		}
+
+		//---------------------------------------------------------------------------------------------------------//
+		//------------------------------------------------Debugging------------------------------------------------//
+		//---------------------------------------------------------------------------------------------------------//
+
+
+		public int getTimes(Point location, Color color) {
+			int sum = 0;
+			Stopwatch s = new Stopwatch();
+			List<long> times = new List<long>();
+			bool blue = true;
+			int x = location.X;
+			int y = location.Y;
+			while (times.Count < 5) {
+				var c = GetPixelColor(location);
+				if (c.R == color.R && c.G == color.G && c.B == color.B) {
+					blue = true;
+					s.Reset();
+					s.Start();
+					Thread.Sleep(200);
+					while (blue) {
+						Color c1 = GetPixelColor(location);
+						if (c1.R == color.R && c1.G == color.G && c1.B == color.B) {
+							s.Stop();
+							times.Add(s.ElapsedMilliseconds);
+							blue = false;
+						}
+					}
+				}
+			}
+			return sum / times.Count();
+		}
+
+		public int getTimesMessageBox(Point locationStartButton) {
+			Color fullRangeCastColor = Color.FromArgb(026, 118, 241);
+			Point rangeAreaPosition = new Point(locationStartButton.X, locationStartButton.Y - 75);
+			if (checkIfUserOnStartScreen(locationStartButton)) {
+				SendKeys.Send("%{Tab}");
+			}
+			Thread.Sleep(100);
+			return getTimes(rangeAreaPosition, fullRangeCastColor);
+		}
+
+		//---------------------------------------------------------------------------------------------------------//
+		//------------------------------------------Auto clicking helpers------------------------------------------//
+		//---------------------------------------------------------------------------------------------------------//
+		public void startCast(Point locationStartButton) {
+			checkIfUserOnStartScreen(locationStartButton);
+			Color fullRangeCastColor = Color.FromArgb(026, 118, 241);
+			Point rangeAreaPosition = new Point(locationStartButton.X, locationStartButton.Y - 75);
+			Thread.Sleep(100);
+			castRod(rangeAreaPosition, fullRangeCastColor);
+		}
+
+		public bool checkIfUserOnStartScreen(Point locationStartButton) {
+			if (GetPixelColor(locationStartButton).Equals(Color.FromArgb(155, 208, 30))) {
+				Cursor.Position = locationStartButton;
+				LeftClick(locationStartButton);
+				return true;
+			} else {
+				//used to view position of cursor when using two different screens
+				Cursor.Position = locationStartButton;
+			}
+			return false;
+		}
+
+		public Point getScreenLocationPoint(Point location) {
+			Screen screen = Screen.AllScreens[screenNumContainingGame];
+			Point correctedLocation = new Point(screen.Bounds.Location.X + location.X, screen.Bounds.Location.Y + location.Y);
+			return correctedLocation;
+		}
+
+		public void castRod(Point location, Color color) {
+			while (true) {
+				var c = GetPixelColor(location);
+				if (c.R == color.R && c.G == color.G && c.B == color.B) {
+					Thread.Sleep(rodType);
+					SendKeys.Send(" ");
+					return;
+				}
+			}
+		}
+		public void catchFish(Point location, Color color) {
+			Cursor.Position = location;
+			while (true) {
+				var c = GetPixelColor(location);
+				if (c.R == color.R && c.G == color.G && c.B == color.B) {
+					SendKeys.Send(" ");
+					return;
+				}
+			}
+		}
+
+		public void fishGotAwayClick(Point location) {
+			Cursor.Position = location;
+			LeftClick(location);
+			Thread.Sleep(1000);
+		}
+
+
+		public void tradeItemThenCloseClick(Point tradeLocation, Point closeLocation) {
+			Cursor.Position = tradeLocation;
+			LeftClick(tradeLocation);
+			Thread.Sleep(500);
+			Cursor.Position = closeLocation;
+			LeftClick(closeLocation);
+			Thread.Sleep(500);
+		}
+
+		public void fishGotAwaySpace() {
+			SendKeys.SendWait(" ");
+			Thread.Sleep(1000);
+		}
+
+
+
+		public void tradeItemThenCloseSpace() {
+			SendKeys.Send(" ");
+			Thread.Sleep(500);
+			SendKeys.Send(" ");
+			Thread.Sleep(500);
+		}
+	}
+}
